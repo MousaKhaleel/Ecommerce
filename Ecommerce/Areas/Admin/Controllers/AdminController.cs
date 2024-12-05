0f186d8b84210fc91777e20650b7f8ed027ee64a
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-12-05 13:21:34 +0200

    admin dashboard

--- Ecommerce/Areas/Admin/Controllers/AdminController.cs
+++ Ecommerce/Areas/Admin/Controllers/AdminController.cs
@@ -9,57 +9,64 @@ using Microsoft.AspNetCore.Mvc;
 namespace Ecommerce.Areas.Admin.Controllers
 {
 	[Area("Admin")]
-	[Authorize(Roles = "Admin")]
-    public class AdminController : Controller
-    {
-        private readonly SignInManager<User> _signInManager;
-        private readonly UserManager<User> _userManager;
-        private readonly AppDbContext _context;
-        public AdminController(SignInManager<User> _signInManager, UserManager<User> _userManager, AppDbContext dbContext)
-        {
-            this._signInManager = _signInManager;
-            this._userManager = _userManager;
-            _context = dbContext;
-        }
-        public IActionResult Index()
-        {
-            return View("AddProductForm");//TODO: replace with dashboard
-        }
-        [HttpGet]
-        public async Task<IActionResult> AddCategory()
-        {
-            return View("AddCategoryForm");
-        }
-        [HttpPost]
-        public async Task<IActionResult> AddCategory(CategoryViewModel categoryVM)
-        {
-            var category = new Category
-            {
-                CategoryName=categoryVM.CategoryName,
-            };
-            await _context.Categories.AddAsync(category);
-            await _context.SaveChangesAsync();
-            return RedirectToAction("Index");
-        }
-        [HttpGet]
-        public async Task<IActionResult> AddProduct()
-        {
-            return View("AddProductForm");
-        }
-        [HttpPost]
-        public async Task<IActionResult> AddProduct(ProductViewModel productVM)
-        {
-            var userId = _userManager.GetUserId(User);
-            var product = new Product
-            {
-                ProductName = productVM.ProductName,
-                ProductDescription = productVM.ProductDescription,
-                ProductPrice = productVM.ProductPrice,
-                Currency = productVM.Currency,
-                StockQuantity = productVM.StockQuantity,
-                //UserId=userId!,
-            };
-            return RedirectToAction("Index");
-        }
-    }
+	//[Authorize(Roles = "Admin")]
+	public class AdminController : Controller
+	{
+		private readonly SignInManager<User> _signInManager;
+		private readonly UserManager<User> _userManager;
+		private readonly AppDbContext _context;
+		public AdminController(SignInManager<User> _signInManager, UserManager<User> _userManager, AppDbContext dbContext)
+		{
+			this._signInManager = _signInManager;
+			this._userManager = _userManager;
+			_context = dbContext;
+		}
+		public IActionResult Index()
+		{
+			return View("Index");//TODO: replace with dashboard
+		}
+		[HttpGet]
+		public async Task<IActionResult> AddCategory()
+		{
+			return View("AddCategoryForm");
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddCategory(CategoryViewModel categoryVM)
+		{
+			var category = new Category
+			{
+				CategoryName = categoryVM.CategoryName,
+			};
+			await _context.Categories.AddAsync(category);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+		[HttpGet]
+		public async Task<IActionResult> AddProduct()
+		{
+			return View("AddProductForm");
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddProduct(ProductViewModel productVM, IFormFile ProductImageFile)
+		{
+			//if (ProductImageFile != null && ProductImageFile.Length > 0)
+			//{
+			//	string imageUrl = await uploadi;
+			//}
+			var userId = _userManager.GetUserId(User);
+			var product = new Product
+			{
+				ProductName = productVM.ProductName,
+				ProductDescription = productVM.ProductDescription,
+				ProductPrice = productVM.ProductPrice,
+				Currency = productVM.Currency,
+				StockQuantity = productVM.StockQuantity,
+				//ProductImage=
+				//UserId=userId!,
+			};
+			await _context.Products.AddAsync(product);
+			await _context.SaveChangesAsync();
+			return RedirectToAction("Index");
+		}
+	}
 }
