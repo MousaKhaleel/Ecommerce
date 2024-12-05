b51a8f186b5434658e7aaefdd9ef9bde50638a57
Mousa Khaleel <mousakhaleel22@gmail.com>
2024-12-05 11:43:59 +0200

    fixed relationships

--- Ecommerce/Data/AppDbContext.cs
+++ Ecommerce/Data/AppDbContext.cs
@@ -5,30 +5,41 @@ using System.Reflection.Metadata;
 
 namespace Ecommerce.Data
 {
-	public class AppDbContext: IdentityDbContext<User>
-	{
-		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    public class AppDbContext : IdentityDbContext<User>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-		public DbSet<Product> Products { get; set; }
-		public DbSet<Cart> Carts { get; set; }
-		public DbSet<ProductCart> productCarts { get; set; }
-		public DbSet<Category> Categories { get; set; }
-		public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<ProductCart> productCarts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
 
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
 
-	//		modelBuilder.Entity<Product>()
-	//.HasOne(b => b.User)
-	//.WithMany(u => u.Cart)
-	//.HasForeignKey(b => b.UserId)
-	//		.OnDelete(DeleteBehavior.Restrict);
+            //		modelBuilder.Entity<Product>()
+            //.HasOne(b => b.User)
+            //.WithMany(u => u.Cart)
+            //.HasForeignKey(b => b.UserId)
+            //		.OnDelete(DeleteBehavior.Restrict);
 
-			modelBuilder.Entity<Product>()
-				.Property(e => e.CreatedDate)
-				.HasDefaultValueSql("GETDATE()");
+            modelBuilder.Entity<User>()
+.HasOne(u => u.Cart)
+.WithOne(c => c.User)
+.HasForeignKey<Cart>(c => c.UserId);
 
-			base.OnModelCreating(modelBuilder);
-		}
-	}
+            modelBuilder.Entity<ProductCart>()
+    .HasKey(tc => new { tc.ProductId, tc.CartId });
+
+            modelBuilder.Entity<ProductCategory>()
+    .HasKey(tc => new { tc.ProductId, tc.CategoryId });
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.CreatedDate)
+                .HasDefaultValueSql("GETDATE()");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
 }
